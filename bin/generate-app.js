62a5eaacd3e0cf4bba21001c2bc004fa3dc5baf9
smarthug <kirklayer@gmail.com>
2024-08-31 05:13:22 +0900

    works

--- bin/generate-app.js
+++ bin/generate-app.js
@@ -14,7 +14,7 @@ if (process.argv.length < 3) {
 const projectName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
-const git_repo = 'https://github.com/Leopold-V/react-parcel-app.git';
+const git_repo = 'https://github.com/smarthug/ultraspace-template.git';
 
 try {
     fs.mkdirSync(projectPath);
